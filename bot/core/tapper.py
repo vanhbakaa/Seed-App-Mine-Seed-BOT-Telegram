76ea5854e62ec48cc3fe8bc0899e7cd249ed0b6f
Vanhbaka <vanhbaka2k7@gmail.com>
2024-10-23 11:18:43 +0700

    Update tapper.py

--- bot/core/tapper.py
+++ bot/core/tapper.py
@@ -652,12 +652,16 @@ class Tapper:
         token_live_time = randint(3500, 3600)
         while True:
             try:
+                if check_base_url() is False:
+                    if settings.ADVANCED_ANTI_DETECTION:
+                        sys.exit(
+                            "Detected index js file change. Contact me to check if it's safe to continue: https://t.me/vanhbakaaa")
+                    else:
+                        sys.exit(
+                            "Detected api change! Stopped the bot for safety. Contact me here to update the bot: https://t.me/vanhbakaaa")
+
                 if time.time() - access_token_created_time >= token_live_time:
                     # logger.info(f"{self.session_name} | Update auth token...")
-                    if check_base_url() is False:
-                        sys.exit(
-                            "Detected api change! Stoped the bot for safety. Contact me here to update the bot: https://t.me/vanhbakaaa")
-
                     tg_web_data = await self.get_tg_web_data(proxy=proxy)
                     headers['telegram-data'] = tg_web_data
                     # print(tg_web_data)
