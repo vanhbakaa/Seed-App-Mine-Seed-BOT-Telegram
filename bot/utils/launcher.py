62946ca7b0b35e2ad6fd979978fc87dea0aecb62
Vanhbaka <vanhbaka2k7@gmail.com>
2024-11-16 12:57:12 +0700

    Fix query bug

--- bot/utils/launcher.py
+++ bot/utils/launcher.py
@@ -59,12 +59,17 @@ def get_proxies() -> list[Proxy]:
 
 def fetch_username(query):
     try:
-        fetch_data = unquote(query).split("user=")[1].split("&auth_date=")[0]
+        fetch_data = unquote(query).split("user=")[1].split("&chat_instance=")[0]
         json_data = json.loads(fetch_data)
         return json_data['username']
     except:
-        logger.warning(f"Invaild query: {query}")
-        sys.exit()
+        try:
+            fetch_data = unquote(query).split("user=")[1].split("&auth_date=")[0]
+            json_data = json.loads(fetch_data)
+            return json_data['username']
+        except:
+            logger.warning(f"Invaild query: {query}")
+            sys.exit()
 
 async def get_user_agent(session_name):
     async with AIOFile('user_agents.json', 'r') as file:
